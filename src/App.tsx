96b5068534c01dd1c545e4f31740234da1faf0ad
senunn” <senuxxx123@gmail.com>
2025-02-26 22:44:10 +0530

    Responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { Button, HStack } from "@chakra-ui/react"
+import React from "react";
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react";
 
 function App() {
+  const isLargeScreen = useBreakpointValue({ base: false, lg: true });
 
   return (
-    <HStack>
-      <Button>Click me</Button>
-    </HStack>
-  )
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show when={isLargeScreen} fallback={null}>
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
