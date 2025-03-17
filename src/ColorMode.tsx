6c09bb7caf0084c4b9bf839a9c25a998f8ed9463
senunn” <senuxxx123@gmail.com>
2025-03-17 19:15:32 +0530

    color mode added

---
+++ src/ColorMode.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ColorModeButton } from './components/ui/color-mode'
+
+
+const ColorMode = () => {
+  return (
+    <div>
+        <ColorModeButton/>
+    </div>
+  )
+}
+
+export default ColorMode
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.png'
+import ColorMode from '@/ColorMode'
 
 const Navbar = () => {
   return (
    <HStack>
     <Image src={logo} boxSize="60px"/>
+    <Text>NavBar</Text>
+    <ColorMode/>
    </HStack>
   )
 }
